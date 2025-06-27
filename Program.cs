a8272a74f325dc5ce9a4226b7953299891c5ce92
Bhoomi <bhoomiahir321@gmail.conm>
2025-06-27 13:17:50 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _5BTECH_A
+{
+     class Program
+    {
+       public static void Main(string[] args)
+        {
+            Console.WriteLine("helloooo");
+            Console.Write("hiii");
+
+            Console.Read();
+        }
+    }
+}
---
+++ sum.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5BTECH_A
+{
+    internal class sum
+    {
+        public static void Main(string[] args)
+        {
+            int a = 5;
+            int b=5;
+            int sum = a + b;
+            Console.WriteLine("sum is " + sum);
+
+            Console.Read();
+        }
+       
+    }
+}
